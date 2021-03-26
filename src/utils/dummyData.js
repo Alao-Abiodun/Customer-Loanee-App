9a39831f5850e3d4a410117524330880c01a0376
Abiodun <abiodundev@gmail.com>
2021-03-26 14:30:46 +0100

    feat:create an API for userProfile and loginUsers

---
+++ src/utils/dummyData.js
@@ -0,0 +1,22 @@
+export default {
+  users: [
+    {
+      id: 1,
+      name: 'Ren Money',
+      description: 'Salary earners discounted loan',
+      interestrate: 3,
+      amount: 50000,
+      startdate: '31/12/2020',
+      enddate: '31/03/2021',
+    },
+    {
+      id: 2,
+      name: 'Kia Kia',
+      description: 'Easy small loan',
+      interestrate: 1.5,
+      amount: 5000,
+      startdate: '31/01/2021',
+      enddate: '30/04/2021',
+    },
+  ],
+};
